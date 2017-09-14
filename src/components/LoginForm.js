555f89c44e284749c2304abb54f13118d0ede639
Alan English <aenglish@manorhillcapital.com>
2017-09-14 13:29:13 -0500

    add nested catch functions for error handling on authorization, if user is not found, create a new account, if new account cannot be created, throw error message, define default error prop as empty string, import text tag, include text tag in render method to show error if it arises

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  // screen can always be re-rendered by using component level state
+  // whenever we use state, we have to default our new property
+  state = { email: '', password: '', error: '' };
 
   onButtonPress() {
     const { email, password } = this.state;
-    
-    firebase.auth().signInWithEmailAndPassword(email, password);
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed.' });
+          });
+      });
   }
 
   render() {
@@ -35,6 +44,10 @@ class LoginForm extends Component {
             onChangeText={password => this.setState({ password })}
           />
         </CardSection>
+
+        <Text>
+          {this.state.error}
+        </Text>
 
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
