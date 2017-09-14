f9efbb262aa93fbd27afc96d7ac772616ae3ccc7
Alan English <aenglish@manorhillcapital.com>
2017-09-13 21:21:00 -0500

    npm install firebase, import firebase library, copy in code from firebase project to locate firebase data bucket

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD8lX7e-RpEr4ImC6L5VM33f2UyhCiVrbE',
+      authDomain: 'authentication-e3994.firebaseapp.com',
+      databaseURL: 'https://authentication-e3994.firebaseio.com',
+      projectId: 'authentication-e3994',
+      storageBucket: 'authentication-e3994.appspot.com',
+      messagingSenderId: '65307407205'
+    });
+  }
   render() {
     return (
       <View>
