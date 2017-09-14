185b6f6c7807c67eecd2b57da541a61a54d1d08f
Alan English <aenglish@manorhillcapital.com>
2017-09-14 13:52:35 -0500

    import activity indicator from react native library, pass in prop of size to reuseable spinner component, set size equal to prop passed in or large, add style object and insert spinner style into view tag that wraps indicator

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
-const Spinner = () => {
+// pass spinner a prop called size, small or large
+const Spinner = ({ size }) => {
   return (
-    <View />
+    <View style={styles.spinnerStyle}>
+      <ActivityIndicator size={size || 'large'} />
+    </View>
   );
-}
+};
+
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
 
 export { Spinner };
